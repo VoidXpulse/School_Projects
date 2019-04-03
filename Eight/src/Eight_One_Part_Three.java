cb244e67617cc4412e3cf2c791953ee4991c0d71
VoidXpulse <voidxpulse@gmail.com>
2019-04-02 20:23:48 -0400

    Sten Laa

--- Eight/src/Eight_One_Part_Three.java
+++ Eight/src/Eight_One_Part_Three.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Eight_One_Part_Three {
     /*
    Justin Ecarma
@@ -6,10 +8,23 @@ public class Eight_One_Part_Three {
 
    */
     public static void main(String[] args) {
+        Scanner theScanner = new Scanner(System.in);
         int ages[] = new int[20];
-        for(int y = 0; y < 20; y++)
+        int count = 0, num;
+        System.out.println("Enter the number of ages you want to enter.");
+        num = theScanner.nextInt();
+        for(int y = 0; y < num; y++)
         {
             System.out.println("Enter an age");
+            ages[y] = theScanner.nextInt();
         }
+
+        for(int x:ages)
+        {
+            if(x > 15){
+                count++;
+            }
+        }
+        System.out.println("The number of ages above 15 is " + count);
     }
 }
