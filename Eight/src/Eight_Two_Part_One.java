6a6019437eafebb125a3def2adcf8b7287920ba3
VoidXpulse <voidxpulse@gmail.com>
2019-04-04 18:19:17 -0400

    annoyed

--- Eight/src/Eight_Two_Part_One.java
+++ Eight/src/Eight_Two_Part_One.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Eight_Two_Part_One {
@@ -5,22 +6,22 @@ public class Eight_Two_Part_One {
         Scanner theScanner = new Scanner(System.in);
         int numbers;
         double sum = 0, perkent;
-
+        DecimalFormat decaFor = new DecimalFormat("0.#");
+        DecimalFormat decaFor2 = new DecimalFormat("0");
         System.out.println("How many numbers do you want to enter?");
         numbers = theScanner.nextInt();
         double usernums[] = new double[numbers];
         System.out.println("Please enter the " + numbers + " numbers.");
-        for(double i:usernums) {
-            i = theScanner.nextInt();
-            sum += i;
+        for(int i = 0; i < usernums.length; ++ i) {
+            usernums[i] = theScanner.nextInt();
+            sum += usernums[i];
         }
 
-        System.out.println("The sum is " + sum + ".");
+        System.out.println("The sum is " + decaFor2.format(sum) + ".");
         System.out.println("Statistics");
-
         for(double y:usernums) {
-            perkent = y / sum;
-            System.out.println(y + " is " + perkent + "% of the sum");
+            perkent = (y / sum) * 100;
+            System.out.println(decaFor2.format(y) + " is " + decaFor.format(perkent) + "% of the sum");
         }
 
     }
