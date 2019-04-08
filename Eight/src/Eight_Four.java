6b8a860c48d8614f2362c959ba2793c893989744
VoidXpulse <voidxpulse@gmail.com>
2019-04-08 17:19:20 -0400

    4/8/2019 Singing Words of Wisdom, Let it be

--- Eight/src/Eight_Four.java
+++ Eight/src/Eight_Four.java
@@ -1,18 +1,38 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Eight_Four {
     //Justin Ecarma
     //4/5/2019
     //Lab 8.4
-    //
+    //Extra:A new set of words gets generated each time you run the program.
     public static void main(String[] args) {
-        int count, howmany;
+        int count, word;
         String key;
         Scanner theScanner = new Scanner(System.in);
-        System.out.println("How many Strings should be in the method");
-        howmany = theScanner.nextInt();
-        String food[] = {"orange", "apple", "cheese", "cake", "roast beef", "orange", "mutton", "grape", "sushi", "ice cream"};
-
+        Random rando = new Random();
+        String food[] = new String[10];
+        for(int i = 0; i < 10; i++) {
+            word = rando.nextInt(10);
+            if(word <= 4) {
+                food[i] = "orange";
+            }
+            else if(word == 5) {
+                food[i] = "apple";
+            }
+            else if(word == 6) {
+                food[i] = "banana";
+            }
+            else if(word == 7) {
+                food[i] = "grape";
+            }
+            else if(word == 8) {
+                food[i] = "pear";
+            }
+            else {
+                food[i] = "blueberry";
+            }
+        }
         System.out.println("Enter the word you would like to search for in the method.");
         key = theScanner.next();
         count = seq_search(food, key);
@@ -21,7 +41,7 @@ public class Eight_Four {
     public static int seq_search(String[] food, String key) {
         int count = 0;
         for(String y:food) {
-            if(y == key){
+            if(y.equals(key)){
                 count++;
             }
         }
