dd855febd8ab808eaddacabb4deb79f159e953c5
VoidXpulse <voidxpulse@gmail.com>
2019-04-26 08:40:18 -0400

    4/26/2019 reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- Eight Part One/src/Eight_Eight.java
+++ Eight Part One/src/Eight_Eight.java
@@ -6,7 +6,7 @@ public class Eight_Eight {
         Scanner theScanner = new Scanner(System.in);
         String[] name = {"Barney Fife", "Luke Skywalker", "Cruella Deville", "Sally Smith", "Marsha Brady", "Gomez Addams", "John Adams", "Marie Antoinette", "Albert Einstein","Mick Jagger", "Mickey Mouse"};
         String[] address = {"10 Warbler Rd.", "R2 Death Star Ave.", "123 Witchey Way", "3862 Street Blvd.", "1970 Groovy Street", "1 Spooky Circle", " 3445 Presidential Plaza", "2 Rue Morgue", "7 Relativity Rd.", "100 Rolling Stone Path", "2 Disney Lane"};
-        String[] city = {"Mountainville, SC 34765", "Jupiter, MD 44623", "Hollywood, CA 12345", "Springfield, IL 31733", "Arlington, VA 51266", "Tombstone, AZ 63341", "New York, NY 11220", "New Orleans, LA 25993", "Topeka KS 63121", "Seattle, WA 23733", "Orlando, FL 72272"};
+        String[] city = {"Mountainville, SC 34765", "Jupiter, MD 44623", "Hollywood, CA 12345", "Springfield, IL 31733", "Arlington, VA 51266", "Tombstone, AZ 63341", "New York, NY 11220", "New Orleans, LA 25993", "Topeka, KS 63121", "Seattle, WA 23733", "Orlando, FL 72272"};
 
         System.out.println("Chose what order you would like to view in.");
         System.out.println("1. Last Name \n2. Name of City \n3. Number of Zipcode");
@@ -19,9 +19,10 @@ public class Eight_Eight {
             namecity(name, address, city);
         }
         else {
-            zip(city, name, address);
+
+            zip(city, address, name);
         }
-        for(int i = 0; i < name.length - 1; i++) {
+        for(int i = 0; i < name.length; i++) {
             System.out.print(name[i]);
             System.out.print(" | ");
             System.out.print(address[i]);
@@ -79,30 +80,22 @@ public class Eight_Eight {
         }
     }
     public static void zip(String[] name, String[] address, String[] city) {
-        String temp, number = "";
-        boolean flag = true;
-        int g;
-        while(flag){
-            flag = false;
+        String temp;
 
-            for(int i = 0; i < name.length - 1; i++) {
+        for(int i = 0; i < city.length - 1; i++) {
+            for(int j = i + 1; j < city.length; j++) {
+                    if(Integer.parseInt(name[i].substring(name[i].indexOf(", ") + 5, (name[i].indexOf(", ") + 10))) > Integer.parseInt(name[j].substring(name[j].indexOf(", ") + 5, (name[j].indexOf(", ") + 10)))) {
+                        temp = city[i];
+                        city[i] = city[j];
+                        city[j] = temp;
 
-                for (g = 0; g < 9; g++)
-                    number = Integer.toString(g);
-                    System.out.println(name[i].substring(name[i].indexOf(" " + number), (name[i].indexOf(" " + number) + 2)));
-                if ((name[i].substring(name[i].indexOf(" " + number), (name[i].indexOf(" " + number) + 2)).compareToIgnoreCase(name[i + 1].substring(name[i + 1].indexOf(" " + number), (name[i + 1].indexOf(" " + number) + 2)))) > 0) {
-                    temp = name[i];
-                    name[i] = name[i + 1];
-                    name[i + 1] = temp;
+                        temp = name[i];
+                        name[i] = name[j];
+                        name[j] = temp;
 
-                    temp = address[i];
-                    address[i] = address[i + 1];
-                    address[i + 1] = temp;
-
-                    temp = city[i];
-                    city[i] = city[i + 1];
-                    city[i + 1] = temp;
-                    flag = true;
+                        temp = address[i];
+                        address[i] = address[j];
+                        address[j] = temp;
                 }
             }
         }
