4e6268582bd88c9d8388d543f8cf3382fabf4e8f
VoidXpulse <voidxpulse@gmail.com>
2019-04-28 13:01:16 -0400

    4/25/2019 So much work so little time

--- Eight Part Two/src/Eight_Nine_Part_One.java
+++ Eight Part Two/src/Eight_Nine_Part_One.java
@@ -1,21 +1,46 @@
+import java.util.Scanner;
+
 public class Eight_Nine_Part_One {
     //Justin Ecarma
     //4/26/2019
     //Lab 8.9
     //
     public static void main(String[] args) {
+        int oscar = -1;
+        Scanner theScanner = new Scanner(System.in);
         String[] words = {"camel", "ant", "dog", "cat", "horse", "mule", "java", "goose"};
-        String[] friends = {"", "", "", "", ""};
+        String[] friends = new String[5];
+        String[] terms = new String[10];
         alphabetic(words);
+        printArray(words);
 
-        for(String y:words) {
-            System.out.println(y);
+        System.out.println("Enter five friend's names.");
+        for(int i = 0; i < friends.length; i++) {
+            friends[i] = theScanner.next();
         }
-
-        do {
-
+        printArray(friends);
+        alphabetic(friends);
+        printArray(friends);
+        oscar = seqSearch(friends);
+        if(oscar != -1) {
+            System.out.println("Oscar is at friends[" + oscar + "].");
         }
-        while(friends[4] == "");
+        else {
+            System.out.println("Oscar not found");
+        }
+        System.out.println("Enter ten computer terms.");
+        for(int i = 0; i < terms.length; i++) {
+            terms[i] = theScanner.next();
+        }
+        printArray(terms);
+        alphabetic(terms);
+        printArray(terms);
+        if (binarySearch(terms, "mouse", terms.length - 1, 0)) {
+            System.out.println("Mouse is in the array.");
+        }
+        else {
+            System.out.println("Mouse isn't in the array.");
+        }
     }
     public static void alphabetic(String[] words) {
         String temp;
@@ -33,4 +58,38 @@ public class Eight_Nine_Part_One {
             }
         }
     }
+    public static void printArray(String[] words) {
+        for(String y:words) {
+            System.out.println(y);
+        }
+    }
+    public static int seqSearch(String[] words) {
+        int oscar = -1;
+        for(int t = 0; t < words.length; t++) {
+            if(words[t].equalsIgnoreCase("Oscar")) {
+                oscar = t;
+            }
+        }
+        return oscar;
+    }
+    public static boolean binarySearch(String numbers[], String key, int top, int bottom) {
+        return binarySearchOne(numbers, key, top, bottom);
+    }
+    public static boolean binarySearchOne(String numbers[], String key, int top, int bottom) {
+        if(bottom > top) {
+            return false;
+        }
+        int mid = (bottom + top) / 2;
+
+        if(numbers[mid].equalsIgnoreCase(key)) {
+            return true;
+        }
+        else if(key.equalsIgnoreCase(numbers[mid])){
+            return binarySearchOne(numbers, key, mid - 1, bottom);
+        }
+        else {
+            return binarySearchOne(numbers, key, top, mid +1);
+        }
+    }
+}
 }
