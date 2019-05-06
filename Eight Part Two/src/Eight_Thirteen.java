276025175b6b2dc15d39c523aaa1cfe76f78e318
VoidXpulse <voidxpulse@gmail.com>
2019-05-06 08:24:12 -0400

    5/6/2019 1 2 3 4 5 6 7 8 9 10 Duel Commandments

--- Eight Part Two/src/Eight_Thirteen.java
+++ Eight Part Two/src/Eight_Thirteen.java
@@ -1,17 +1,44 @@
+import java.util.Scanner;
+
 public class Eight_Thirteen {
     //Justin Ecarma
     //5/2/2019
     //Lab 8.13
-    //
+    //Extra: User can make their own square
     public static void main(String[] args) {
+        Scanner theScanner = new Scanner(System.in);
+        int x = 0;
         int magic[][] = {{16, 3, 2, 13}, {5, 10, 11, 8}, {9, 6, 7, 12}, {4, 15, 14, 1}};
-
-
-        if(check(magic)) {
-            System.out.println("This square is indeed magic.");
+        int model = 0;
+        System.out.println("Would you like to do a premade array or make one yourself?");
+        System.out.println("1. Premade");
+        System.out.println("2. Original");
+        model = theScanner.nextInt();
+        if(model <= 1) {
+            if(check(magic)) {
+                System.out.println("This square is indeed magic.");
+            }
+            else {
+                System.out.println("This magic square is fake.");
+            }
         }
         else {
-            System.out.println("This magic square is fake.");
+            System.out.println("What side length would you like?");
+            x = theScanner.nextInt();
+            int test[][] = new int[x][x];
+
+            for (int i = 0; i < test.length; i++) {
+                for (int j = 0; j < test.length; j++) {
+                    test[i][j] = theScanner.nextInt();
+                }
+            }
+
+            if(check(magic)) {
+                System.out.println("This square is indeed magic.");
+            }
+            else {
+                System.out.println("This magic square is fake.");
+            }
         }
     }
     public static boolean check(int[][] magic) {
@@ -53,5 +80,6 @@ public class Eight_Thirteen {
         if(tempsum != yeet) {
             fail = false;
         }
+        return fail;
     }
 }
