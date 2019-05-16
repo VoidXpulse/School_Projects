88c75462b8dda150a6a07ad9cc93062e1e354fc3
VoidXpulse <voidxpulse@gmail.com>
2019-05-16 08:32:03 -0400

    5/16/2019 Garish

---
+++ Eight Part Three/src/Eight_Twenty.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Eight_Twenty {
+    //Justin Ecarma
+    //
+    //
+    //
+    public static void main(String[] args) {
+        File payroll = new File("src/payroll.txt");
+    }
+}
